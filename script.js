1164620d88988a83eec44c80088c646c380871a9
juanvg1529 <rockjdvg1529@gmail.com>
2021-04-13 19:16:05 -0500

    2 files modified

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('hello!')
+console.log("hello!");
+/*sumar dos numeros, agregarle el 20% y dividir entre 3 */
+function aritmetica(x, y) {
+  let sum;
+  sum = x + y;
+  console.log(`la suma de los numero es ${sum}`);
+  sum = sum + sum * 0.2;
+  console.log(`al agregar el 20% es ${sum}`);
+  sum = sum / 3;
+  console.log(`al dividir por 3`);
+}
+aritmetica(2, 3);
